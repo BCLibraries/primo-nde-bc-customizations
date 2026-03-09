8ea615bb746667cd410035e7b8d1484673bd4800
Scot Colford <scot.colford@bc.edu>
2026-03-09 13:40:42 -0400

    add hathi trust compoent

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,8 +1,10 @@
 import { NoResultsComponent } from '../no-results/no-results.component';
 import { ProxyBorrowerComponent } from '../proxy-borrower/proxy-borrower.component';
+import { HathiTrustComponent } from '../hathi-trust/hathi-trust.component';
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
   ['nde-search-no-results', NoResultsComponent],
   ['nde-personal-details-view-after', ProxyBorrowerComponent],
+  ['nde-online-availability-before', HathiTrustComponent],
 ]);
