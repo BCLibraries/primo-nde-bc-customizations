1294214947b84dbc9920a88839e336c3f5b9f428
Scot Colford <scot.colford@bc.edu>
2026-03-12 13:54:30 -0400

    add brief display styling

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,1 +1,21 @@
-console.log('Hello from custom.js');
+var GTM_ID = "GTM-5R4S8V4"; /* GA/GTM Container ID for environment */
+
+/* Google Tag Manager */
+const gtmId = GTM_ID;
+function addGTM(doc) {
+  const newScript = doc.createElement("script");
+  const scriptText = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`;
+  newScript.innerHTML = scriptText;
+  doc.head.append(newScript);
+
+  const noscript = doc.createElement("noscript");
+  const noscriptText = `&lt;iframe src="//www.googletagmanager.com/ns.html?id=${gtmId}"
+height="0" width="0" style="display:none;visibility:hidden"&gt;&lt;/iframe&gt;`;
+  noscript.innerHTML = noscriptText;
+  doc.body.insertBefore(noscript, doc.body.firstChild);
+}
+addGTM(document);
